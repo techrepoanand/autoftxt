39f9fc9b0bb06ecb1729855e8a255619243c3b9b
techrepoanand <166824753+techrepoanand@users.noreply.github.com>
2024-04-14 17:17:31 +0530

    Update and rename sample_config.py to config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+
+class Config(object):
+    
+    DOWNLOAD_LOCATION = "./DOWNLOADS"
+
+    BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+
+    APP_ID = int(os.environ.get("APP_ID", "28156285"))
+
+    API_HASH = os.environ.get("API_HASH", "93694f0328daa5b515c9523a0d0dd86d")    
+    
+    CAPTION_TEXT = os.environ.get("CAPTION_TEXT", "@WatchingCenter")
+
+    CAPTION_POSITION = os.environ.get("CAPTION_POSITION", "bottom")
+
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "6777494089").split())
